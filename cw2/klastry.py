cf3a5dfbbf65a04f1edf0a96fcb01becbc7f0b4c
mm_mor <mm_mor@onet.pl>
2023-03-20 17:17:35 +0100

    klastry

--- cw2/klastry.py
+++ cw2/klastry.py
@@ -13,7 +13,7 @@ with open('LidarData.xyz', 'r', encoding='utf-8') as csvfile:
 points = [x for x in zip(*[iter(points)]*3)]
 points = np.array(points, dtype=float)
 x, y, z = zip(*points)
-fig = plt.figure()
+
 ax = plt.axes(projection='3d')
 ax.scatter3D(x, y, z)
 plt.title('points clouds in 3D', fontsize=14)
@@ -21,6 +21,31 @@ plt.tight_layout
 plt.xlabel('x', fontsize=12)
 plt.ylabel('y', fontsize=12)
 ax.set_zlabel('z', fontsize=12)
+
+
+
+clusterer = KMeans(n_clusters=3)
+clusterer.fit(points)
+y_pred = clusterer.predict(points)
+z_pred = clusterer.predict(points)
+
+red = y_pred == 0
+blue = y_pred == 1
+cyan = y_pred == 2
+
+red_z = z_pred == 0
+blue_z = z_pred == 1
+cyan_z = z_pred == 2
+
+#plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter3D(points[red, 0], points[red, 1], points[cyan_z, 2], c='red')
+ax.scatter3D(points[blue, 0], points[blue, 1], points[cyan_z, 2], c='blue')
+ax.scatter3D(points[cyan, 0], points[cyan, 1], points[cyan_z, 2], c='cyan')
+# plt.scatter(points[red, 0], points[red, 1], c='red')
+# plt.scatter(points[blue, 0], points[blue, 1], c='blue')
+# plt.scatter(points[cyan, 0], points[cyan, 1], c='cyan')
+
 plt.show()
 
 
